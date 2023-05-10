17c879d33a3e5519f68e8c22fed4c12492ae453a
Alvaro Barcena <alvarobarcena27@gmail.com>
2023-05-10 12:44:50 +0200

    Actualizado del menu

--- PROYECTOINTEGRADOR/src/appapuestas/MenuPrincipal.java
+++ PROYECTOINTEGRADOR/src/appapuestas/MenuPrincipal.java
@@ -9,6 +9,10 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MenuPrincipal extends JFrame {
 
@@ -23,11 +27,38 @@ public class MenuPrincipal extends JFrame {
 	public MenuPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnEquipo = new JMenu("Equipos");
+		menuBar.add(mnEquipo);
+		
+		JMenuItem mntmMostrarJugadores = new JMenuItem("Mostrar Jugadores");
+		mnEquipo.add(mntmMostrarJugadores);
+		
+		JMenuItem mntmDiferenciaGoles = new JMenuItem("Diferencia de goles");
+		mnEquipo.add(mntmDiferenciaGoles);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Mostrar Saldo");
+		mnEquipo.add(mntmNewMenuItem);
+		
+		JMenu mnJugadores = new JMenu("Jugadores");
+		menuBar.add(mnJugadores);
+		
+		JMenu mnNewMenu_2 = new JMenu("New menu");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenu mnNewMenu_3 = new JMenu("New menu");
+		menuBar.add(mnNewMenu_3);
+		
+		JMenu mnNewMenu_4 = new JMenu("New menu");
+		menuBar.add(mnNewMenu_4);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
 	}
 
 }
