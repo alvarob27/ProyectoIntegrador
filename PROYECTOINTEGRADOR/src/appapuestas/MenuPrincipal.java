0bcb75a6d3c7440f5ac46649909b2576c0c3cee0
Alvaro Barcena <alvarobarcena27@gmail.com>
2023-05-10 12:44:08 +0200

    Actualizado del menu

--- PROYECTOINTEGRADOR/src/appapuestas/MenuPrincipal.java
+++ PROYECTOINTEGRADOR/src/appapuestas/MenuPrincipal.java
@@ -1,10 +1,33 @@
 package appapuestas;
 
-public class MenuPrincipal {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class MenuPrincipal extends JFrame {
+
+	private JPanel contentPane;
+	static Login frame = new Login();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		frame.setVisible(true);
 
+	}
+	
+	public MenuPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
